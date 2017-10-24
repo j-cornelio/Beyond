734cc9ac9308d1a15f981ae37e4240ee927d5e29
julio <empjulio@hotmail.com>
2017-10-24 11:11:21 -0500

    new shortent component installed

--- src/js/components/main/Shorten.js
+++ src/js/components/main/Shorten.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-function ellipse(str, length){
-  if(length > 200){
+function ellipse(str, bool){
+  if(bool){
     return str.slice(0, 200) + '...';
     } else {
     return str;
@@ -9,15 +9,25 @@ function ellipse(str, length){
 }
 
 class Shorten extends Component{
-  _show(str, length){
-    console.log(str, length)
+  constructor(props){
+    super(props);
+
+    this.state = {
+      toggleText: true
+    }
+  }
+  _showHide(str, length){
+    this.setState({toggleText: !this.state.toggleText})
+    console.log('show func: ', str, this.state.toggleText);
+
+    ellipse(str, this.state.toggleText)
   }
 
 	render(){
     var { str } = this.props;
    return (
-      <span onClick={this._show.bind(null, str, str.length)}>
-        {ellipse(str, str.length)}
+      <span onClick={this._showHide.bind(this, str)}>
+        {ellipse(str, str.length > 200)}
       </span>
     ) 
   }
--- src/js/components/main/Video.js
+++ src/js/components/main/Video.js
@@ -1,6 +1,7 @@
 import React      from 'react';
 import { Link }   from 'react-router';
 import Shorten    from './Shorten';
+import ShowMore   from 'react-show-more';
 
 function lessMore(str, less){
   if(less){
@@ -12,14 +13,23 @@ function lessMore(str, less){
 
 const Video = ({snippet, id, show}) => {
 
-  var thumbs = snippet.thumbnails ? snippet.thumbnails.medium.url : 'none';
+  var thumbs = (snippet.thumbnails) ? snippet.thumbnails.medium.url : '../images/no-video.png';
 
 	return (
     <div className="row video-row">
       <div className="col-sm-6 col-sm-push-6">
         <h3>{snippet.title}</h3>
         <h4>Published on {snippet.publishedAt.slice(0, 10)}</h4>
-        <p className="hidden-xs"><Shorten str={snippet.description} /></p>
+        <div className="hidden-xs">
+
+          <ShowMore
+            lines={6}
+            more='Show more'
+            less='Show less'
+          >
+            {snippet.description}
+          </ShowMore>
+        </div>
       </div>
 
       <div className="col-sm-6 col-sm-pull-6">
