77fc0777cc9ef1d157932bfac6bdbbc99b7f7d87
julio <empjulio@hotmail.com>
2017-10-26 14:16:33 -0500

    bug bug

--- src/js/components/HOC/LoaderHOC.js
+++ src/js/components/HOC/LoaderHOC.js
@@ -6,13 +6,13 @@ const LoaderHOC = (propName) => (WrappedComponent) => {
 	return class LoaderHOC extends Component{
 		isEmpty(prop){
 			return (
-				prop===null || prop===undefined || (prop.hasOwnProperty('length') && prop.length===0 ||
+				prop===null || prop===undefined || prop.hasOwnProperty('length') && prop.length===0 ||
 					prop.constructor===Object && Object.keys(prop).length===0
 			)
 		}
 
 	    render(){
-console.log('props---', this.props)
+console.log('props---', this.props);
 	      return this.isEmpty(this.props[propName]) ? <h1>LOADER</h1> : <WrappedComponent {...this.props} />
 	    }
 	}
